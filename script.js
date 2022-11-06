8c2a0ea737bf6678abcbc0e86d88249569658cbd
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2022-11-06 21:39:36 +0500

    Two users game

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 let turn='X';
-let tar
+let tar;
+let boxes=document.querySelectorAll('td');
+
 let changeTurn=()=>{
     turn==='X'?turn='O':turn='X';
 }
 
-document.querySelectorAll('td').forEach(element => {
+let checkWin=()=>{
+    let wins=[
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    wins.forEach(element => {
+        if(boxes[element[0]].innerText===boxes[element[1]].innerText&&boxes[element[1]].innerText===boxes[element[2]].innerText&&boxes[element[2]].innerText!=''){
+            alert(`${turn} has won the Game.`)
+            return
+        }
+    });
+}
+
+boxes.forEach(element => {
     element.addEventListener('click', (e)=>{
         tar=e.target.innerText
-        e.target.innerText?changeTurn():e.target.innerText=turn, changeTurn();
+        e.target.innerText?void(0):(e.target.innerText=turn, checkWin(), changeTurn());
     })
     
 });
